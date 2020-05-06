3f6289e1e9a7522b966b3b10800b72a7bbceee61
johny anastacio <johnyanastaciods@gmail.com>
2020-05-05 21:09:03 -0300

    projeto integrador

--- main.js
+++ main.js
@@ -99,20 +99,45 @@ function totalCost(product){
 function displayCart(){
     let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
-    let productContainer = document.querySelector('.products');
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
     if(cartItems && productContainer){
+        
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item =>{
             productContainer.innerHTML += `
             <div class="product">
-                <ion-icon name="close-circle"></ion-icon>
+                <ion-icon  class="remove-button" name="close-circle"></ion-icon>
                 <img src="./imagens/${item.tags}.jpg">
                 <span>${item.name}</span>
             </div>
+            <div class="price">${item.price}</div>
+            <div class="quantity">
+            <ion-icon name="arrow-back-circle-outline"></ion-icon>
+            <span>${item.inCart}</span>
+            <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price}.00
+            </div>
             ` 
-        })
+    })
+    productContainer.innerHTML +=`
+    <div class="basketTotalContainer">
+      <h4 class="basketTotalTitle">
+        Basket Total
+      </h4>
+      <h4 class="basketTotal">
+        $${cartCost},00
+      </h4>
+
+    </div>`
     }
 
     }
+
+    
+
 onLoadCartNumbers();
 displayCart();
